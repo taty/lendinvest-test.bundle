f3320030ce73ca04b75b9289779bc26aa0db8a0a
tatyanavakulenko <tvakulenko@gmail.com>
2018-03-01 16:31:13 +0200

    Add calculate income test

--- tests/InvestorTest.php
+++ tests/InvestorTest.php
@@ -10,4 +10,17 @@ class InvestorTest extends TestCase
 
     }
 
+    public function testCalculateIncome()
+    {
+        $loan = new Loan('01-10-2015', '15-11-2015');
+
+        $tranch1 = new Tranch();
+        $tranch1->setPercent(3);
+        $tranch1->setMaxAvailable('1000');
+
+        $inv1 = new Investor('Investor 1');
+        $inv1->setTranch($loan, $tranch1, 1000, '03-10-2015');
+
+        $this->assertEquals(28.06, $inv1->calculateIncome('01-11-2015'));
+    }
 }
