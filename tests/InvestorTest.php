a473cc9a30572a8381b2301d50a8f63b2cb6e905
tatyanavakulenko <tvakulenko@gmail.com>
2018-03-01 16:49:39 +0200

    adds second testcase

--- tests/InvestorTest.php
+++ tests/InvestorTest.php
@@ -10,7 +10,7 @@ class InvestorTest extends TestCase
 
     }
 
-    public function testCalculateIncome()
+    public function testCalculateIncomeOne()
     {
         $loan = new Loan('01-10-2015', '15-11-2015');
 
@@ -22,5 +22,19 @@ class InvestorTest extends TestCase
         $inv1->setTranch($loan, $tranch1, 1000, '03-10-2015');
 
         $this->assertEquals(28.06, $inv1->calculateIncome('01-11-2015'));
+    }
+
+    public function testCalculateIncomeTwo()
+    {
+        $loan = new Loan('01-10-2015', '15-11-2015');
+
+        $tranch2 = new Tranch();
+        $tranch2->setPercent(6);
+        $tranch2->setMaxAvailable('1000');
+
+        $inv3 = new Investor('Investor 3');
+        $inv3->setTranch($loan, $tranch2, 500, '10-10-2015');
+
+        $this->assertEquals(21.29, $inv3->calculateIncome('01-11-2015'));
     }
 }
